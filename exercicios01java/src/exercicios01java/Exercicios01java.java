9451c9a8276ca4d6a0369856b0f56f25f573ba48
unknown <diego_f_arruda@estudante.sesisenai.org.br>
2024-07-13 18:44:17 -0300

    Primeiro modulo completo

---
+++ exercicios01java/src/exercicios01java/Exercicios01java.java
@@ -0,0 +1,15 @@
+
+package exercicios01java;
+
+
+public class Exercicios01java {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Olá Mundo!");
+        
+        
+    }
+    
+}
