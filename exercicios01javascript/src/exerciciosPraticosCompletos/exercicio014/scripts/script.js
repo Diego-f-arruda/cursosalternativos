3011c0601d737f0b8270d2ebedf7ca39882c326a
unknown <diego_f_arruda@estudante.sesisenai.org.br>
2024-10-18 18:57:15 -0300

    Pagina hora do dia pronta

--- exercicios01javascript/src/exerciciosPraticosCompletos/exercicio014/scripts/script.js
+++ exercicios01javascript/src/exerciciosPraticosCompletos/exercicio014/scripts/script.js
@@ -7,13 +7,13 @@ var hora = data.getHours();
  
 msg.innerHTML = `Agora são ${hora} horas`
 if(hora >= 0 && hora < 12){
-    imagem.src = `manha.jpg`
+    imagem.src = `imagens/manha.png`
     document.body.style.background = `#e2cd9f`
 }else if(hora >= 12 && hora < 18){
-    imagem.src = `tarde.jpg`
+    imagem.src = `imagens/tarde.png`
     document.body.style.background = `#b9846f`
 }else{
-    imagem.src = `noite.jpg`
+    imagem.src = `imagens/noite.png`
     document.body.style.background = `#515154`
 }
 }
